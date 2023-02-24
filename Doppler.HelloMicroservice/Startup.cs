e57765581458f34c3ad16ca0c488b59d877de35c
Andres Moschini <andresmoschini@gmail.com>
2023-02-24 14:19:17 -0300

    chore: replace ambiguous name preparing for .net7

--- Doppler.HelloMicroservice/Startup.cs
+++ Doppler.HelloMicroservice/Startup.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Hellang.Middleware.ProblemDetails;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -23,7 +22,11 @@ public class Startup
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-        services.AddProblemDetails();
+        // Explicitly using Hellang because services.AddProblemDetails() generates an ambiguity
+        // between Microsoft.AspNetCore.Http.Extensions.ProblemDetailsServiceCollectionExtensions
+        // and Hellang.Middleware.ProblemDetails.ProblemDetailsExtensions
+        // TODO: consider replace Hellang by out of the box alternative (but it is not working fine right now)
+        Hellang.Middleware.ProblemDetails.ProblemDetailsExtensions.AddProblemDetails(services);
         services.AddDopplerSecurity();
         services.AddControllers();
         services.AddCors();
@@ -64,7 +67,7 @@ public class Startup
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-        app.UseProblemDetails();
+        Hellang.Middleware.ProblemDetails.ProblemDetailsExtensions.UseProblemDetails(app);
 
         app.UseSwagger();
         app.UseSwaggerUI(c => c.SwaggerEndpoint("v1/swagger.json", "Doppler.HelloMicroservice v1"));
